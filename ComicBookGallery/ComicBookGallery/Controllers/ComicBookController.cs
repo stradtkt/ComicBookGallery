ce99ada1acaf4ee05c1c198b370114394e430c02
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-07 19:38:01 -0500

    Updated detail action types

---
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller!");
+        }
+    }
+}
